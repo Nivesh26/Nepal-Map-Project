37718b032595761e3348a87df2eaea3d91219cfa
Nivesh26 <nivesh.shrestha.94@gmail.com>
2026-08-09 18:54:00 +0545

    feat: implement react-router-dom and create Home and Nepal components

---
+++ src/Components/Nepal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nepal: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[50vh] p-8">
+      <h2 className="text-3xl font-bold text-white mb-4">Nepal Map Component</h2>
+      <p className="text-gray-300 text-lg">
+        This is where the Nepal Map will be displayed.
+      </p>
+    </div>
+  );
+};
+
+export default Nepal;
